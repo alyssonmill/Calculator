c4c1e3a650f7fd0095cf04dd421f244ac5e6a6a6
Alysson Miller <alyssonmillerdev@gmail.com>
2024-03-29 09:21:54 +0000

    Ending calculator

--- src/script.js
+++ src/script.js
@@ -1,11 +1,48 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//ESCREVA SEU CÓDIGO AQUI!!
 
+let result = 0;
+
+function Calculadora(n1,n2,op) {
+
+  switch (op) {
+      case "+":
+        result = n1 + n2;
+      break;
+      case "-":
+        result = n1 - n2;
+      break;
+      case "*":
+        result = n1 * n2;
+      break;
+      case "/":
+        result = n1 / n2;
+      break;
+      case "e":
+        // Entrada => num1 = 2, num2 = 3; Saída => resultado = 2*2*2 = 8  
+
+      for(let i = 0; i < n2; i++){
+        result += n1;
+      }
+
+
+      break;
+    default:
+      break;
+  }
+
+  return result ;
+
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
 
 let visor = document.getElementById("visor");
 
@@ -18,7 +55,7 @@ function limpezatotal() {
 }
 
 function calcular() {
-  let numeros = visor.value.split(/[^0-9]/)
+  let numeros = visor.value.split(/[^0-9]/).map(Number)
   let operadores = visor.value.split(/[0-9]/)
   let operador
   operadores.forEach(x => {
